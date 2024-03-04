6a4a08a96a1f30b14ab9cb72da18c220bf711667
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2024-03-04 12:50:41 +0100

    Adding constants

---
+++ Constants.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from math import pi
+from Vehicle import VehicleEmulation
+from anki import TrackPieceType
+
+@dataclass(slots=True)
+class Constants:
+    StaightLength: int = 550
+    IntersectionLength: int = 550
+    FinishLength: int = 35
+    StartLength: int = 20
+    
+    def curveRadius(roadOffset:int) -> int:
+        return 0.5*pi*(roadOffset+280)
+    
+    def pieceLength(vehicle: VehicleEmulation) -> int:
+        match vehicle.current_track_piece.type:
+            case TrackPieceType.START:
+                return Constants.StaightLength
+            case TrackPieceType.FINISH:
+                return Constants.FinishLength
+            case TrackPieceType.INTERSECTION:
+                return Constants.IntersectionLength
+            case TrackPieceType.CURVE:
+                return Constants.curveRadius(vehicle.road_offset)
+
+    
+    def timeUntilTrackpieceChange(vehicle: VehicleEmulation, distance:int) -> float:
+        return distance / vehicle.speed
