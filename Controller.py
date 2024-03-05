a8301a52d65381a4b6c1c90b33c58d672693222b
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2024-03-05 12:05:57 +0100

    Changing controller and vehicle for a lower level limulation approach

--- Controller.py
+++ Controller.py
@@ -1,187 +1,38 @@
-from anki import Controller, TrackPiece
+from anki import Controller, TrackPiece, errors
+from anki.control.vehicle import BatteryState
 from typing import Collection, Optional
 from anki.control.scanner import BaseScanner
 import asyncio
+
 from Vehicle import VehicleEmulation
 from Scanner import DummyScanner
 
 
 class ControllerEmulation(Controller):
-    def __init__(self, *, timeout: float=10):
-        self.timeout: float = timeout
-        self.vehicles: set[VehicleEmulation] = set()
-        self.map: Optional[list[TrackPiece]] = None
-    
-    async def connect_one(self,vehicle_id: Optional[int]=None) -> VehicleEmulation:
-        """Connect to one non-charging Supercar and return the Vehicle instance
-
-        :param vehicle_id: :class:`Optional[int]`
-            The id given to the :class:`Vehicle` instance on connection
-
-
-        Returns
-        -------
-        :class:`Vehicle`
-            The connected supercar
-
-
-        Raises
-        ------
-        :class:`VehicleNotFound`
-            No supercar was found in the set timeout
-        
-        :class:`ConnectionTimedoutException`
-            The connection attempt to the supercar did not succeed within the set timeout
-        
-        :class:`ConnectionDatabusException`
-            A databus error occured whilst connecting to the supercar
-        
-        :class:`ConnectionFailedException`
-            A generic error occured whilst connection to the supercar
-
-        :class:`RuntimeError`
-            A vehicle with the specified id already exists.
-            This will only be raised when using a custom id.
-        """
-        vehicle = VehicleEmulation(self._check_vehicle_id(vehicle_id),self)
-        vehicle._road_offset = 0
-        self.vehicles.add(vehicle)
-        return vehicle
-    
-    async def connect_specific(
-            self, 
-            address: str, 
-            vehicle_id: Optional[int]=None
+    async def _get_vehicle(
+        self,
+        vehicle_id: Optional[int]=None,
+        address: str|None=None
         ) -> VehicleEmulation:
-        """Connect to a supercar with a specified MAC address
-        
-        :param address: :class:`str`
-            The MAC-address of the vehicle to connect to. Needs to be uppercase seperated by colons
-        :param vehicle_id: :class:`int`
-            The id passed to the :class:`Vehicle` object on its creation
+        # Finds a Supercar and creates a Vehicle instance around it
 
-        Returns
-        -------
-        :class:`Vehicle`
-            The connected supercar
-        
-        Raises
-        ------
-        :class:`VehicleNotFound`
-            No supercar was found in the set timeout
-        
-        :class:`ConnectionTimedoutException`
-            The connection attempt to the supercar did not succeed within the set timeout
-        
-        :class:`ConnectionDatabusException`
-            A databus error occured whilst connecting to the supercar
-        
-        :class:`ConnectionFailedException`
-            A generic error occured whilst connection to the supercar
-
-        :class:`RuntimeError`
-            A vehicle with the specified id already exists.
-            This will only be raised when using a custom id.
-        """
-        return await self.connect_one(vehicle_id)
-    
-    async def connect_many(
-            self,
-            amount: int,
-            vehicle_ids: Collection[int|None]|None=None
-    ) -> tuple[VehicleEmulation, ...]:
-        """Connect to <amount> non-charging Supercars
-        
-        :param amount: :class:`int`
-            The amount of vehicles to connect to
-        :param vehicle_ids: :class:`Optional[Iterable[int]]`
-            The vehicle ids passed to the :class:`Vehicle` instances
-
-        Returns
-        -------
-        :class:`tuple[Vehicle]`
-            The connected supercars
-
-        Raises
-        ------
-        :class:`ValueError`
-            The amount of requested supercars does not match the length of :param vehicle_ids:
-
-        :class:`VehicleNotFound`
-            No supercar was found in the set timeout
-
-        :class:`ConnectionTimedoutException`
-            A connection attempt to one of the supercars timed out
-
-        :class:`ConnectionDatabusException`
-            A databus error occured whilst connecting to a supercar
-
-        :class:`ConnectionFailedException`
-            A generic error occured whilst connecting to a supercar
-        
-        :class:`RuntimeError`
-            A vehicle with the specified id already exists.
-            This will only be raised when using a custom id.
-        """
-        if vehicle_ids == None:
-            vehicle_ids = []
-        while len(vehicle_ids) < amount:
-            vehicle_ids.append(None)
-
-        return tuple([await self.connect_one(id) for id in vehicle_ids])
-    
-    async def scan(
-            self,
-            scan_vehicle: VehicleEmulation|None=None,
-            /,
-            align_pre_scan: bool=True,
-            scanner_class: type[BaseScanner] = DummyScanner
-    ) -> list[TrackPiece]:
-        """Assembles a digital copy of the map and adds it to every connected vehicle.
-        
-        :param scan_vehicle: :class:`Optional[Vehicle]`
-            When passed a Vehicle object, this Vehicle will be used as a scanner. Otherwise one
-            will be selected automatically.
-        :param align_pre_scan: :class:`bool`
-            When set to True, the supercars can start from any position on the map and align
-            automatically before scanning. Disabling this means your supercars need to start
-            between START and FINISH
-        :param scanner_class: :class:`type`
-            The type of scanner used
-
-        Returns
-        -------
-        :class:`list[TrackPiece]`
-            The resulting map
-
-        Raises
-        ------
-        :class:`DuplicateScanWarning`
-            The map was already scanned in. This scan will be skipped.
-        """
-        scanner = scanner_class(scan_vehicle)
-        
-        if align_pre_scan:
-            # Aligning before scanning if enabled.
-            # This allows the vehicles to be placed anywhere on the map
-            await asyncio.gather(*[scanner.align(v) for v in self.vehicles])
-            # Since we're aligning BEFORE scan, we need the piece before
-            # the one we want to align in front of
-            await asyncio.sleep(1)
-        await asyncio.sleep(1)#Todo: add dalay
-        for v in self.vehicles:
-            v._map = self.map
-            v._position = len(self.map) - 1 if v in self.vehicles else 0
-            # Scanner is always one piece ahead
-        return self.map
-
-    
-    def _check_vehicle_id(self, vehicle_id: int| None):
         vehicle_ids = {v.id for v in self.vehicles}
-        if vehicle_id == None:
+        if vehicle_id is None:
+            # Automatically assign generate unused vehicle id
             vehicle_id = 1024
             while vehicle_id in vehicle_ids:
                 vehicle_id += 1
+                pass
+            pass
         elif vehicle_id in vehicle_ids:
             raise RuntimeError(f"Duplicate id for vehicle. Id {vehicle_id} already in use.")
-        return vehicle_id
+
+        vehicle = VehicleEmulation(
+            vehicle_id,
+            None,
+            None,
+            self,
+            battery = BatteryState(True,False,False,False)
+        )
+        self.vehicles.add(vehicle)
+        return vehicle
--- Vehicle.py
+++ Vehicle.py
@@ -1,53 +1,44 @@
-from warnings import warn
-from anki import Vehicle, TrackPiece, BaseLane, TrackPieceType
-from anki.control.vehicle import BatteryState
+from anki import Vehicle, TrackPiece, errors
+from anki.control.vehicle import BatteryState, _call_all_soon
+from anki.misc import msg_protocol, const
 import asyncio
-from typing import Optional, Callable
-from Constants import Constants
-from threading import Event, Thread
-from time import sleep, perf_counter
+import struct
 
-_Callback = Callable[[], None]
 
-def waitingTask(time: int):
-    if time == float("inf"):
-        while(True):
-            sleep(100)
-    else:
-        sleep(time)
-
-def interuptTask(changedEvent:Event):
-    changedEvent.wait()
-    changedEvent.clear()
+def vehicleTread():
+    while True:
+        pass
 
 class VehicleEmulation(Vehicle):
-    def __init__(
-            self,
-            id: int,
-            controller: Optional["ControllerEmulatior"]=None,  # Inconsistent, but fixes failing docs
-            *,
-            battery: BatteryState | None = None
-    ):
-        super().__init__(id,None,None,controller,battery=BatteryState)
-        self._is_connected = True
-        
-        self._pieceDistanceLeft = 0
-        self._changedEvent = Event()
-        self._VehicleThread = Thread(target=vehicleThread,daemon=True,args=(self,))
-        self._VehicleThread.start()
-    
-    async def wait_for_track_change(self) -> Optional[TrackPiece]:
-        """Waits until the current track piece changes.
-        
-        Returns
-        -------
-        :class:`TrackPiece`
-            The new track piece. `None` if :func:`Vehicle.map` is None
-            (for example if the map has not been scanned yet)
-        """
-        await self._track_piece_future
-        # Wait on a new track piece (See _notify_handler)
-        return self.current_track_piece
+    async def _Vehicle__send_package(self, payload: bytes):#Overrides Vehicle.__send_package
+        """Send a payload to the supercar"""
+        if self._write_chara is None:
+            raise RuntimeError("A command was sent to a vehicle that has not been connected.")
+        try:
+            packetType, content =  msg_protocol.disassemble_packet(payload)
+            match packetType:
+                case const.ControllerMsg.SET_SPEED:
+                    speed, accel = struct.unpack_from("<ii",payload)
+                    self._speed = speed
+                case const.ControllerMsg.CHANGE_LANE:
+                    (
+                        horizontalSpeed,
+                        horizontalAcceleration,
+                        roadCenterOffset,
+                        _hopIntent,
+                        _tag
+                    ) = struct.unpack_from("<HHfBB",payload)
+                    self._road_offset = roadCenterOffset
+                case const.ControllerMsg.TURN_180:
+                    raise NotImplementedError("Turn_180 is not supported jet")
+                    pass#TODO: add support
+                case _:
+                    pass
+        except OSError as e:
+            raise RuntimeError(
+                "A command was sent to a vehicle that is already disconnected"
+            ) from e
+
 
     async def connect(self):
         """Connect to the Supercar
@@ -62,8 +53,8 @@ class VehicleEmulation(Vehicle):
         :class:`ConnectionFailedException`
             A generic error occured whilst connection to the supercar
         """
-        await asyncio.sleep(1)#TODO add delay
         self._is_connected = True
+        self._ping_task = asyncio.create_task(self._auto_ping())
     
     async def disconnect(self) -> bool:
         """Disconnect from the Supercar
@@ -85,77 +76,5 @@ class VehicleEmulation(Vehicle):
         :class:`DisconnectFailedException`
             The attempt to disconnect from the supercar failed for an unspecified reason
         """
-        asyncio.sleep(1)#TODO: add delay
         self._is_connected = False
-        return True
-
-    async def set_speed(self, speed: int, acceleration: int = 500):
-        """Set the speed of the Supercar in mm/s
-
-        :param speed: :class:`int`
-            The speed in mm/s
-        :param acceleration: :class:`Optional[int]`
-            The acceleration in mm/s²
-        """
-        self._speed = speed
-        self._changedEvent.set()
-    
-    async def stop(self):
-        """Stops the Supercar"""
-        self._speed = 0
-    
-    async def change_position(
-            self,
-            roadCenterOffset: float,
-            horizontalSpeed: int = 300,
-            horizontalAcceleration: int = 300,
-            *,
-            _hopIntent: int = 0x0,
-            _tag: int = 0x0
-        ):
-        self._road_offset = roadCenterOffset
-    
-    #TODO make this do something 
-    async def turn(self, type: int = 3, trigger: int = 0):
-        # type and trigger don't work correcty
-        """
-        .. warning::
-            This does not yet function properly. It is advised not to use this method
-        """
-        if self.map is not None:
-            warn(
-                "Turning around with a map! This will cause a desync!",
-                UserWarning
-            )
-    
-    async def align(
-            self,
-            speed: int=300,
-            *,
-            target_previous_track_piece_type: TrackPieceType = TrackPieceType.FINISH
-        ):
-        await asyncio.sleep(1)#TODO add delay
-        self._position = 0
-        await self.stop()
-    
-    async def ping(self):
-        """
-        .. warning::
-            Due to a bug in the firmware, supercars will never respond to pings!
-        
-        Send a ping to the vehicle
-        """
-
-def vehicleThread(vehicle: VehicleEmulation):
-    while(vehicle.is_connected):
-        time = perf_counter()
-        oldSpeed = vehicle.speed
-        tasks = (waitingTask(Constants.timeUntilTrackpieceChange(vehicle,vehicle._pieceDistanceLeft)),interuptTask())
-        done, _ = asyncio.wait(tasks,return_when=asyncio.FIRST_COMPLETED)
-        print("vehicle action")
-        if done is tasks[0]:
-            vehicle.map_position = (vehicle.map_position + 1)%len(vehicle.map)
-            vehicle._pieceDistanceLeft = Constants.pieceLength()
-        else:
-            passedTime = perf_counter - time
-            vehicle._pieceDistanceLeft -= oldSpeed*passedTime
+        return self._is_connected
