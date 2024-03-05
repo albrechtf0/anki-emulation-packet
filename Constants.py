de28559007fe23b67928d1ea061bf214dc763ec8
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2024-03-05 10:26:25 +0100

    fixing error with constants

--- Constants.py
+++ Constants.py
@@ -2,16 +2,18 @@ from dataclasses import dataclass
 from math import pi
 from anki import TrackPieceType
 
-@dataclass(slots=True)
+
 class Constants:
     StaightLength: int = 550
     IntersectionLength: int = 550
     FinishLength: int = 35
     StartLength: int = 20
     
+    @staticmethod
     def curveRadius(roadOffset:int) -> int:
         return 0.5*pi*(roadOffset+280)
     
+    @staticmethod
     def pieceLength(vehicle: "VehicleEmulation") -> int:
         match vehicle.current_track_piece.type:
             case TrackPieceType.START:
@@ -23,7 +25,7 @@ class Constants:
             case TrackPieceType.CURVE:
                 return Constants.curveRadius(vehicle.road_offset)
 
-    
+    @staticmethod
     def timeUntilTrackpieceChange(vehicle: "VehicleEmulation", distance:int) -> float:
         if vehicle.speed == 0:
             return float("inf")
