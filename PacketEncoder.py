f7a0dfb7c5bb4589c148c5cc10d3c04435782fbe
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2024-03-06 12:05:20 +0100

    Completing packetEncoder

--- PacketEncoder.py
+++ PacketEncoder.py
@@ -1,44 +1,43 @@
 from anki.misc import msg_protocol, const
 from anki.misc.track_pieces import TrackPieceType
-from Vehicle import VehicleEmulation
 import struct
 
-class PacketDecoder:
-    def encodePacket(vehicle: VehicleEmulation,packetType: int) -> bytes:
-        match packetType:
-            case const.VehicleMsg.CHARGER_INFO:
-                payload = struct.pack(
-                    "<????",
-                    False, 
-                    vehicle.battery_state.on_charger,
-                    vehicle.battery_state.charging,
-                    vehicle.battery_state.full_battery
-                    )
-                return msg_protocol.assemble_packet(packetType.to_bytes(1,"little"),payload)
-            case const.VehicleMsg.TRACK_PIECE_UPDATE:
-                payload = struct.pack(
-                    "<BBfHB",
-                    vehicle.current_track_piece.loc, 
-                    vehicle.current_track_piece.type, 
-                    vehicle.road_offset, 
-                    vehicle.speed, 
-                    vehicle.current_track_piece.clockwise
+def encodePacket(vehicle: "VehicleEmulation",packetType: int) -> bytes:
+    control:"ControllerEmulation" = vehicle._controller
+    match packetType:
+        case const.VehicleMsg.CHARGER_INFO:
+            payload = struct.pack(
+                "<????",
+                False, 
+                vehicle.battery_state.on_charger,
+                vehicle.battery_state.charging,
+                vehicle.battery_state.full_battery
                 )
-                return msg_protocol.assemble_packet(packetType.to_bytes(1,"little"),payload)
-            case const.VehicleMsg.TRACK_PIECE_CHANGE:
-                payload = struct.pack(
-                    "<bbfBBHbBBBBB",
-                    0,
-                    0,
-                    vehicle.road_offset,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    uphill_count, 
-                    downhill_count,
-                    0,
-                    0
-                )
-                return msg_protocol.assemble_packet(packetType.to_bytes(1,"little"),payload)
+            return msg_protocol.assemble_packet(packetType.to_bytes(1,"little"),payload)
+        case const.VehicleMsg.TRACK_PIECE_UPDATE:
+            payload = struct.pack(
+                "<BBfHB",
+                control._simmulatedTrack[vehicle._internalPosition].trackPiece.loc, 
+                control._simmulatedTrack[vehicle._internalPosition].trackPiece.type, 
+                vehicle.road_offset, 
+                vehicle.speed, 
+                control._simmulatedTrack[vehicle._internalPosition].trackPiece.clockwise
+            )
+            return msg_protocol.assemble_packet(packetType.to_bytes(1,"little"),payload)
+        case const.VehicleMsg.TRACK_PIECE_CHANGE:
+            payload = struct.pack(
+                "<bbfBBHbBBBBB",
+                0,
+                0,
+                vehicle.road_offset,
+                0,
+                0,
+                0,
+                0,
+                0,
+                control._simmulatedTrack[vehicle._internalPosition].uphill_count, 
+                control._simmulatedTrack[vehicle._internalPosition].downhill_count,
+                0,
+                0
+            )
+            return msg_protocol.assemble_packet(packetType.to_bytes(1,"little"),payload)
